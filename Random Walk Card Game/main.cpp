bd9fc33abd8731ab90e0089ad727620e926e08ce
Anil Aksu <aaa293@cornell.edu>
2024-03-28 08:00:55 +0300

    Modified to enable the user to play as many times as they want

--- Random Walk Card Game/main.cpp
+++ Random Walk Card Game/main.cpp
@@ -6,7 +6,7 @@
 */
 
 #include <iostream>
-#include<cstdlib>
+#include <cstdlib>
 #include <vector>
 #include <algorithm>  // this header file contains next_permutation function
 
@@ -15,6 +15,8 @@ using namespace std;
 // Function Prototypes
 void getNumberofCards(int& N);
 void checkEven(int N);
+void checkAnswer(string& answer);
+void continuePlaying(string& answer);
 int factorial(int n);
 void getRandomCards(int* cards, int N);
 int getMaxPayoff(int* cards, int N);
@@ -22,31 +24,48 @@ int getMaxPayoff(int* cards, int N);
 int main() {
 
 	int N;						 // Number or cards
-	getNumberofCards(N);		 // Here we get number of cards from the user
+	bool keepPlaying = true;     // Here we enable the end user keep playing
+	string answer;				 // It is the answer from the user to choose to play or not to play 
 
-	int* cards = new int[N];     // Random green and red cards
-	getRandomCards(cards, N);    // Here we create number of random green and red cards
+
 	
 	//for ( int i = 0; i < N ; i++)
 	//	cout << (cards[i] ? "Red" : "Green") << endl;
 
-	cout << "Your maximum payoff is: " << getMaxPayoff(cards, N) << endl;
+	while (keepPlaying)
+	{
+		getNumberofCards(N);		 // Here we get number of cards from the user
+		int* cards = new int[N];     // Random green and red cards
+		getRandomCards(cards, N);    // Here we create number of random green and red cards
+
+		cout << "Your maximum payoff is: " << getMaxPayoff(cards, N) << endl; // Maximum payoff output
+		delete[] cards;				// Here we delete the cards array so that the end user can set the number of cards again
+		cards = nullptr;			// Here we set it to null pointer to avoid memory leakage
+
+		continuePlaying(answer);	// Here we check if the user wants to keep playing
+
+		if( answer == "Yes")
+			keepPlaying = true;
+		else
+			keepPlaying = false;
+	}
+	
 
 	return 0;
 }
 
-void checkEven(int N) // Throws exceptions
+void checkEven(int N)				// Throws exceptions for the number of cards
 {
 
 	if (N % 2 == 1)
-		throw out_of_range("Odd number passed in!");
+		throw invalid_argument("Odd number passed in!");
 	else 
 		cout << "You have " << N << " cards!" << endl;
 
 
 }
 
-void getNumberofCards(int& N)		// Rethrows exceptions
+void getNumberofCards(int& N)		// Checks how many cards the user wants to play with
 {
 	cout << "Please enter even number for the amount of cards!" << endl;
 	cin >> N;
@@ -55,10 +74,37 @@ void getNumberofCards(int& N)		// Rethrows exceptions
 	{
 		checkEven(N);
 	}
-	catch (const out_of_range& err)
+	catch (const invalid_argument& err)
 	{
 		cout << err.what() << endl;
 		getNumberofCards(N);
+	}
+}
+
+void checkAnswer(string& answer) // Throws exceptions for the user deciding to play or not
+{
+
+	if (answer == "Yes")
+		cout << "Here you go!" << endl;
+	else if (answer == "No")
+		cout << "Too bad! you could have earned more, see you later!" << endl;
+	else
+		throw invalid_argument("Please say Yes or No!");
+}
+
+void continuePlaying(string& answer)		// Checks if the user wants to play again
+{
+	cout << "Do you want to continue playing? (Yes/No)" << endl;
+	cin >> answer;
+
+	try
+	{
+		checkAnswer(answer);
+	}
+	catch (const invalid_argument& err)
+	{
+		cout << err.what() << endl;
+		continuePlaying(answer);
 	}
 }
 
